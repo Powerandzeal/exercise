d66db425e3d677ef8e342ad9534dcd665335f534
sereg <seregapanin23703@gmail.com>
2023-03-03 13:37:00 +0300

    для создания пулл реквеста

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,26 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+        changeArr(arr);
+
     }
+
+    public static int [] changeArr (int [] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i]++;
+            }
+        }
+        int [] arr1 = Arrays.copyOfRange(arr,2,6);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
+
+        return arr1;
+    }
+
 }
